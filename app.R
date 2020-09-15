15087bb4559472094c66eb3189bab242bf7d44b2
Lena Mangold <lena@mangold.org>
2020-09-15 09:03:47 -0500

    fix dsmodules functions

--- app.R
+++ app.R
@@ -24,13 +24,13 @@ source("sankey_functions.R")
 # Define UI for data download app ----
 ui <- panelsPage(useShi18ny(),
                  langSelectorInput("lang", position = "fixed"),
-                 panel(title = ui_("upload_data"), 
+                 panel(title = ui_("upload_data"),
                        width = 200,
                        body = uiOutput("dataInput")),
-                 panel(title = ui_("dataset"), 
+                 panel(title = ui_("dataset"),
                        width = 300,
                        body = uiOutput("dataset")),
-                 panel(title = ui_("options"), 
+                 panel(title = ui_("options"),
                        width = 250,
                        color = "chardonnay",
                        body = uiOutput("controls")),
@@ -49,75 +49,72 @@ ui <- panelsPage(useShi18ny(),
 
 # Define server logic ----
 server <- function(input, output) {
-  
-  i18n <- list(defaultLang = "en", 
+
+  i18n <- list(defaultLang = "en",
                availableLangs = c("en", "de", "es", "pt"))
-  
+
   lang <- callModule(langSelector, "lang", i18n = i18n, showSelector = FALSE)
-  
+
   observeEvent(lang(),{
     uiLangUpdate(input$shi18ny_ui_classes, lang())
     })
-  
+
   output$dataInput <- renderUI({
     choices <- c("sampleData", "pasted", "fileUpload", "googleSheets")
     names(choices) <- i_(c("sample", "paste", "upload", "google"), lang = lang())
-    tableInputUI("initial_data", 
+    tableInputUI("initial_data",
+                 "Input data",
                  choices = choices,
                  selected =  "sampleData")
   })
-  
+
   labels <- reactive({
     sm_f <- paste0("data/", i_(c("sample_titanic", "sample_elections"), lang()))
     names(sm_f) <- i_(c("sample_titanic_name", "sample_elections_name"), lang())
-    
-    list(sampleLabel = i_("sample_lb", lang()), 
+
+    list(sampleLabel = i_("sample_lb", lang()),
          sampleFiles = sm_f,
          sampleSelected = sm_f[1],
-         
-         pasteLabel = i_("paste", lang()), 
-         pasteValue = "", 
-         pastePlaceholder = i_("paste_pl", lang()), 
+
+         pasteLabel = i_("paste", lang()),
+         pasteValue = "",
+         pastePlaceholder = i_("paste_pl", lang()),
          pasteRows = 5,
-         
+
          uploadLabel = i_("upload_lb", lang()),
-         uploadButtonLabel = i_("upload_bt_lb", lang()), 
+         uploadButtonLabel = i_("upload_bt_lb", lang()),
          uploadPlaceholder = i_("upload_pl", lang()),
-         
+
          googleSheetLabel = i_("google_sh_lb", lang()),
          googleSheetValue = "",
          googleSheetPlaceholder = i_("google_sh_pl", lang()),
          googleSheetPageLabel = i_("google_sh_pg_lb", lang())
     )
   })
-  
-  inputData <- eventReactive(labels(), {
-    do.call(callModule, c(tableInput,
-                          "initial_data",
-                          labels()))
-  })
-  
+
+  inputData <- tableInputServer("initial_data", labels())
+
   output$dataset <- renderUI({
-    if (is.null(inputData())) 
+    if (is.null(inputData()))
       return()
     suppressWarnings(hotr("hotr_input", data = inputData(), options = list(height = 470)))
   })
-  
+
   data_fringe <- reactive({
     suppressWarnings( hotr::hotr_fringe(input$hotr_input))
   })
-  
+
   dic_load <- reactive({
     data_fringe()$dic
   })
-  
+
   data_load <- reactive({
     data <- data_fringe()$data
     names(data) <- dic_load()$label
     as.data.frame(data)
   })
-  
-  
+
+
   path <- "parmesan"
   parmesan <- parmesan_load(path)
   parmesan_input <- parmesan_watch(input, parmesan)
@@ -128,20 +125,20 @@ server <- function(input, output) {
                   input = input,
                   output = output,
                   env = environment())
-  
-  
+
+
   datasetColumnChoices <- reactive({
     dic_load()$label
   })
-  
+
   datasetColumnSelected <- reactive({
     dic_load()$label[1:2]
   })
-  
+
   useFillValue <- reactive({
     if(!is.null(input$fillval)) TRUE else FALSE
   })
-  
+
   colourMethodChoices <- reactive({
     colour_method_choices <- list("colourpalette" = "colourpalette", "custom" = "custom")
     names(colour_method_choices) <- i_(names(colour_method_choices), lang())
@@ -153,7 +150,7 @@ server <- function(input, output) {
     names(stratum_method_choices) <- i_(names(stratum_method_choices), lang())
     stratum_method_choices
   })
-  
+
   colourPaletteChoices <- reactive({
     list(
       "Categorical:" = list("Accent", "Dark2", "Paired", "Pastel1",
@@ -165,33 +162,33 @@ server <- function(input, output) {
                            "RdPu", "Reds", "YlGn", "YlGnBu", "GlOrBr", "YlOrRD")
     )
   })
-  
+
   colourCustomChoices <- reactive({
     rep("#000000", length(categoriesFill()))
-  }) 
-  
+  })
+
   maxCustomChoices <- reactive({
     length(categoriesFill())
   })
-  
+
   customColours <- reactive({
     colours <- paste0("#",input$colour_custom)
     names(colours) <- sort(categoriesFill())
     colours
-  }) 
-  
+  })
+
   categoriesFill <- reactive({
     data_load() %>% select(input$fillval) %>% distinct() %>% pull()
   })
-  
+
   plot_data <- reactive({
-    data <- prepare_data(df = data_load(), 
-                 col_vars = input$chooseColumns, 
+    data <- prepare_data(df = data_load(),
+                 col_vars = input$chooseColumns,
                  fill_var = input$fillval)
     data <- data %>% mutate(fill = as.character(fill))
     data
     })
-  
+
   plot <- reactive({
     req(input$chooseColumns)
     palette = input$palette
@@ -202,7 +199,7 @@ server <- function(input, output) {
       palette <- NULL
       manualcols <- customColours()
     }
-    create_sankey_plot(df = plot_data(), 
+    create_sankey_plot(df = plot_data(),
                        stratum_colour = input$stratumColour,
                        fill_var = input$fillval,
                        palette = palette,
@@ -219,9 +216,9 @@ server <- function(input, output) {
     dw <- i_("download", lang())
     downloadImageUI("download_data_button", dw, formats = c("jpeg", "png"))
   })
-  
-  callModule(downloadImage, "download_data_button", graph = plot(), lib = "ggplot", formats = c("jpeg", "png"))
-  
+
+  downloadImageServer("download_data_button", element = plot(), lib = "ggplot", formats = c("jpeg", "png"))
+
 }
 
 # Create Shiny app ----
